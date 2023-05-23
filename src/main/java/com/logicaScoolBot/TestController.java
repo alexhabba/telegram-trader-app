6722dd89c59b8aaed0515b09139cd3d4b7ebe7cd
Stanislav_Khabibulin <Stanislav.Khabibulin@rosbank.ru>
2023-05-23 14:42:08 +0300

    add rest allstudent

--- src/main/java/com/logicaScoolBot/TestController.java
+++ src/main/java/com/logicaScoolBot/TestController.java
@@ -1,16 +1,21 @@
 package com.logicaScoolBot;
 
 import com.logicaScoolBot.entity.Payment;
+import com.logicaScoolBot.entity.Student;
 import com.logicaScoolBot.repository.PaymentRepository;
+import com.logicaScoolBot.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
 
     private final PaymentRepository paymentRepository;
+    private final StudentRepository studentRepository;
 
     @GetMapping("/")
     public String test() {
@@ -18,5 +23,10 @@ public class TestController {
                 .map(Payment::getAmount)
                 .map(Integer::valueOf)
                 .reduce(0, Integer::sum).toString();
+    }
+
+    @GetMapping("/allUser")
+    public List<Student> getAllUser() {
+        return studentRepository.findAll();
     }
 }
